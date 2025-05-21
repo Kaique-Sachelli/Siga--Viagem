d561c7b78119fa7c48baa7307b7c152dffd3c616
Gustavo <ghmsilva21@gmail.com>
2025-05-21 19:41:49 -0300

    Adiciona classe DAO

---
+++ src/main/java/Persistencia/DAO.java
@@ -0,0 +1,34 @@
+
+package Persistencia;
+
+import java.sql.ResultSet;
+
+public class DAO {
+    public boolean existe (Usuario u) throws Exception {
+        //try-with-resources 
+        //1.Definir o comando SQL
+        var sql = "SELECT login, senha FROM tb_usuario WHERE login=? AND senha = ?";
+        //2. Estabelecer uma conexão com o SQBD (MySQL)
+        
+        //3.Preparar o comando 
+        try(
+            var conexao = new ConnectionFactory().obterConexao();  
+            var ps = conexao.prepareStatement(sql);
+            
+        ){
+           //4. Substituir os eventuais placeholders 
+            ps.setString(1,u.getLogin());
+            ps.setString(2, u.getSenha());
+             //5. Executar o comando 
+             try(
+                 ResultSet rs = ps.executeQuery();
+                     
+            ){
+                 return rs.next ();
+            }
+             //6. Fechar os recursos
+        }
+       
+    }
+}
+
