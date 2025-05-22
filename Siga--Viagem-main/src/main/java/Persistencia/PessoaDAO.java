1706e9eafea798b2969dcb7b9ef6ac52a1e7ba88
ERICK <erickkentamae@gmail.com>
2025-05-22 20:29:04 -0300

    Criando Telas

---
+++ Siga--Viagem-main/src/main/java/Persistencia/PessoaDAO.java
@@ -0,0 +1,31 @@
+package java;
+
+import java.sql.PreparedStatement;
+
+public class PessoaDAO { //data access object
+    public void cadastrar(Pessoa p) throws Exception{
+        
+        //1. Obter uma conexão com o SGBD
+        var fabricaDeConexoes = new ConnectionFactory();
+        var conexao = fabricaDeConexoes.obterConexao();
+        
+        //2. Definir o comando SQL (INSERT)
+        var sql = "INSERT INTO tb_pessoa (nome, fone, email) VALUES (?, ?, ?)";
+        
+        //3. Preparar o comando por meio da conexão
+        PreparedStatement ps = conexao.prepareStatement(sql);
+        
+        //4. Substituir os eventuais placeholders
+        ps.setString(1, p.getNome());
+        ps.setString(2, p.getFone());
+        ps.setString(3, p.getEmail()); 
+        
+        //5. Executar o comando
+        ps.execute();
+        
+        //6. Fechar os recursos
+        ps.close();
+        conexao.close();
+    }
+    
+}
