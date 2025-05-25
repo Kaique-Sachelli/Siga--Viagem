31f33cf4a49d7d3d749415462a02f8d16a750416
Kaique <kaique.safefe@gmail.com>
2025-05-25 19:07:49 -0300

    Adicionando tela Visao Geral

--- src/main/java/SimulacaoCabine/TelaVisaoGeral.java
+++ src/main/java/SimulacaoCabine/TelaVisaoGeral.java
@@ -2,15 +2,15 @@ package SimulacaoCabine;
 
 import javax.swing.JFrame;
 
-public class TelaAreaExterna extends javax.swing.JFrame {
+public class TelaVisaoGeral extends javax.swing.JFrame {
 
     private JFrame frame;
     
-    public TelaAreaExterna() {
+    public TelaVisaoGeral() {
         initComponents();
     }
 
-    public TelaAreaExterna(JFrame frame) {
+    public TelaVisaoGeral(JFrame frame) {
         this();
         frame.setVisible(false);
         this.frame = frame;
@@ -22,12 +22,8 @@ public class TelaAreaExterna extends javax.swing.JFrame {
     private void initComponents() {
 
         SetaEsquerdaButton1 = new javax.swing.JButton();
-        SetaCimaButton1 = new javax.swing.JButton();
-        AreaExternaLabel = new javax.swing.JLabel();
-
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("Tela Área Externa");
-        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        SetaEsquerdaButton2 = new javax.swing.JButton();
+        VisaoGeralLabel = new javax.swing.JLabel();
 
         SetaEsquerdaButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/SimulacaoImagens/SetaEsquerda.png"))); // NOI18N
         SetaEsquerdaButton1.setContentAreaFilled(false);
@@ -38,21 +34,24 @@ public class TelaAreaExterna extends javax.swing.JFrame {
                 SetaEsquerdaButton1ActionPerformed(evt);
             }
         });
-        getContentPane().add(SetaEsquerdaButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 270, -1, -1));
 
-        SetaCimaButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/SimulacaoImagens/SetaCima.png"))); // NOI18N
-        SetaCimaButton1.setContentAreaFilled(false);
-        SetaCimaButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        SetaCimaButton1.setPreferredSize(new java.awt.Dimension(100, 100));
-        SetaCimaButton1.addActionListener(new java.awt.event.ActionListener() {
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Tela Visão Geral");
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        SetaEsquerdaButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/SimulacaoImagens/SetaEsquerda.png"))); // NOI18N
+        SetaEsquerdaButton2.setContentAreaFilled(false);
+        SetaEsquerdaButton2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        SetaEsquerdaButton2.setPreferredSize(new java.awt.Dimension(100, 100));
+        SetaEsquerdaButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                SetaCimaButton1ActionPerformed(evt);
+                SetaEsquerdaButton2ActionPerformed(evt);
             }
         });
-        getContentPane().add(SetaCimaButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 0, -1, -1));
+        getContentPane().add(SetaEsquerdaButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 270, -1, -1));
 
-        AreaExternaLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/SimulacaoImagens/Sinalização externa de portas fechadas 4 (1).jpg"))); // NOI18N
-        getContentPane().add(AreaExternaLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 960, 640));
+        VisaoGeralLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/SimulacaoImagens/13 - Visão geral do carro 5 (1).jpg"))); // NOI18N
+        getContentPane().add(VisaoGeralLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 960, 640));
 
         pack();
         setLocationRelativeTo(null);
@@ -63,10 +62,10 @@ public class TelaAreaExterna extends javax.swing.JFrame {
         this.dispose();
     }//GEN-LAST:event_SetaEsquerdaButton1ActionPerformed
 
-    private void SetaCimaButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SetaCimaButton1ActionPerformed
-        TelaVisaoGeral janelaVisaoGeral = new TelaVisaoGeral(this);
-        janelaVisaoGeral.setVisible(true);
-    }//GEN-LAST:event_SetaCimaButton1ActionPerformed
+    private void SetaEsquerdaButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SetaEsquerdaButton2ActionPerformed
+        frame.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_SetaEsquerdaButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -85,27 +84,27 @@ public class TelaAreaExterna extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TelaAreaExterna.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaVisaoGeral.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TelaAreaExterna.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaVisaoGeral.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TelaAreaExterna.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaVisaoGeral.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(TelaAreaExterna.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaVisaoGeral.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new TelaAreaExterna().setVisible(true);
+                new TelaVisaoGeral().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel AreaExternaLabel;
-    private javax.swing.JButton SetaCimaButton1;
     private javax.swing.JButton SetaEsquerdaButton1;
+    private javax.swing.JButton SetaEsquerdaButton2;
+    private javax.swing.JLabel VisaoGeralLabel;
     // End of variables declaration//GEN-END:variables
 }
