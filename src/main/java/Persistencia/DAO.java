2652eb42808b18db1f7b6477ef80b4d89a228bbf
Gustavo <ghmsilva21@gmail.com>
2025-06-06 21:49:11 -0300

    Adiconado método de atualização de senha no banco

--- src/main/java/Persistencia/DAO.java
+++ src/main/java/Persistencia/DAO.java
@@ -104,7 +104,18 @@ public class DAO {
             return null;
         }
     }
-
-    
-    
+    public boolean atualizaSenha(String senha, int id) throws Exception{
+        var sql = "UPDATE usuario SET senha = ? WHERE id_usuario = ?";
+        try(
+                
+            var conexao = new ConnectionFactory().obterConexao();  
+            var ps = conexao.prepareStatement(sql)
+        ){
+            ps.setString(1, senha);
+            ps.setInt(2, id);
+            
+            int atualizou = ps.executeUpdate();
+            return atualizou > 0; 
+        }
+    }
 }
