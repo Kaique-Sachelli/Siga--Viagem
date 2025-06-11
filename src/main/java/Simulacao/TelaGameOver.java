e6e24d1cc910e6847b5e4ebe13ebc7f69f22ff62
Guilherme Visone <guivisone@gmail.com>
2025-06-11 14:06:53 -0300

    Resolvendo bug de GameOver

--- src/main/java/Simulacao/TelaGameOver.java
+++ src/main/java/Simulacao/TelaGameOver.java
@@ -30,7 +30,6 @@ public class TelaGameOver extends javax.swing.JFrame {
         acertosLabel = new javax.swing.JLabel();
         gameOverLabel = new javax.swing.JLabel();
         reiniciarButton = new javax.swing.JButton();
-        pontuacaoLabel = new javax.swing.JLabel();
         erroFatalLabel = new javax.swing.JLabel();
         voltarButton = new javax.swing.JButton();
         acertosLabel1 = new javax.swing.JLabel();
@@ -65,12 +64,6 @@ public class TelaGameOver extends javax.swing.JFrame {
         });
         getContentPane().add(reiniciarButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 400, 270, 80));
 
-        pontuacaoLabel.setFont(new java.awt.Font("Impact", 0, 36)); // NOI18N
-        pontuacaoLabel.setForeground(new java.awt.Color(255, 255, 255));
-        pontuacaoLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        pontuacaoLabel.setText("Sua pontuação foi de: " + Integer.toString(Utilidades.Pontuacao.calcularPontuacao() + Utilidades.Pontuacao.calcularErros()));
-        getContentPane().add(pontuacaoLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 190, 960, 70));
-
         erroFatalLabel.setFont(new java.awt.Font("Impact", 0, 36)); // NOI18N
         erroFatalLabel.setForeground(new java.awt.Color(255, 255, 255));
         erroFatalLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
@@ -89,17 +82,17 @@ public class TelaGameOver extends javax.swing.JFrame {
         });
         getContentPane().add(voltarButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 500, 270, 80));
 
-        acertosLabel1.setFont(new java.awt.Font("Impact", 0, 24)); // NOI18N
+        acertosLabel1.setFont(new java.awt.Font("Impact", 0, 36)); // NOI18N
         acertosLabel1.setForeground(new java.awt.Color(255, 255, 255));
         acertosLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         acertosLabel1.setText("Sua quantidade de acertos foi de: " + Integer.toString(Utilidades.Pontuacao.calcularAcertos()));
-        getContentPane().add(acertosLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 260, 960, 70));
+        getContentPane().add(acertosLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 210, 960, 70));
 
-        errosLabel.setFont(new java.awt.Font("Impact", 0, 24)); // NOI18N
+        errosLabel.setFont(new java.awt.Font("Impact", 0, 36)); // NOI18N
         errosLabel.setForeground(new java.awt.Color(255, 255, 255));
         errosLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         errosLabel.setText("Sua quantidade de erros foi de: " + Integer.toString(Utilidades.Pontuacao.contarErrosCometidos()));
-        getContentPane().add(errosLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 320, 960, 70));
+        getContentPane().add(errosLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 290, 960, 70));
 
         imagemGameOverLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/SimulacaoImagens/TelaGameOver.png"))); // NOI18N
         getContentPane().add(imagemGameOverLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 960, 640));
@@ -171,7 +164,6 @@ public class TelaGameOver extends javax.swing.JFrame {
     private javax.swing.JLabel errosLabel;
     private javax.swing.JLabel gameOverLabel;
     private javax.swing.JLabel imagemGameOverLabel;
-    private javax.swing.JLabel pontuacaoLabel;
     private javax.swing.JButton reiniciarButton;
     private javax.swing.JButton voltarButton;
     // End of variables declaration//GEN-END:variables
